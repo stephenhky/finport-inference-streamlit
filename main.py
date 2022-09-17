45c6bbef110967bf9c283f366ba916f69184ea86
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-09-17 16:14:48 -0400

    added a button

--- main.py
+++ main.py
@@ -66,55 +66,57 @@ symbol = st.sidebar.selectbox(
 i_startdate = st.sidebar.date_input('Start Date', value=date(2021, 1, 6))
 i_enddate = st.sidebar.date_input('End Date', value=date.today())
 
-index = '^GSPC'
-startdate = i_startdate.strftime('%Y-%m-%d')
-enddate = i_enddate.strftime('%Y-%m-%d')
+if st.sidebar.button('Compute!'):
 
-# starting asyncio
-task_estimate_symbols = get_symbol_estimations(symbol, startdate, enddate, index)
-task_values_over_time = get_symbol_plot_data(symbol, startdate, enddate)
+    index = '^GSPC'
+    startdate = i_startdate.strftime('%Y-%m-%d')
+    enddate = i_enddate.strftime('%Y-%m-%d')
 
-# estimation
-symbol_estimate = asyncio.run(task_estimate_symbols)
-r = symbol_estimate['r']
-sigma = symbol_estimate['vol']
-downside_risk = symbol_estimate['downside_risk']
-upside_risk = symbol_estimate['upside_risk']
-beta = symbol_estimate['beta']
+    # starting asyncio
+    task_estimate_symbols = get_symbol_estimations(symbol, startdate, enddate, index)
+    task_values_over_time = get_symbol_plot_data(symbol, startdate, enddate)
 
-# making portfolio and time series
-worthdf, plot_url, spreadsheet_url = asyncio.run(task_values_over_time)
+    # estimation
+    symbol_estimate = asyncio.run(task_estimate_symbols)
+    r = symbol_estimate['r']
+    sigma = symbol_estimate['vol']
+    downside_risk = symbol_estimate['downside_risk']
+    upside_risk = symbol_estimate['upside_risk']
+    beta = symbol_estimate['beta']
 
-# display
-col1, col2 = st.columns((2, 1))
+    # making portfolio and time series
+    worthdf, plot_url, spreadsheet_url = asyncio.run(task_values_over_time)
 
-# plot
-f = plt.figure()
-f.set_figwidth(10)
-f.set_figheight(8)
-plt.xlabel('Date')
-plt.ylabel('Portfolio Value')
-stockline, = plt.plot(worthdf['TimeStamp'], worthdf['stock_value'], label='stock', linewidth=0.75)
-totalline, = plt.plot(worthdf['TimeStamp'], worthdf['value'], label='stock+dividend', linewidth=0.75)
-xticks, _ = plt.xticks(rotation=90)
-step = len(xticks) // 10
-plt.xticks(xticks[::step])
-plt.legend([stockline, totalline], ['stock', 'stock+dividend'])
-col1.pyplot(f, height=30)
+    # display
+    col1, col2 = st.columns((2, 1))
 
-# inference
-col2.title('Inference')
-col2.write('yield = {:.4f} (annually {:.2f}%)'.format(r, convert_expreturn_to_annualreturn(r)*100))
-col2.write('volatility = {:.4f}'.format(sigma))
-col2.write('downside risk = {:.4f}'.format(downside_risk))
-col2.write('upside risk = {:.4f}'.format(upside_risk))
-if beta is not None:
-    col2.write('beta (w.r.t. {}) = {:.4f}'.format(index, beta))
-col2.write('Name: {}'.format(allsymbol_info[symbol]['description']))
-col2.markdown('Symbol: [{sym:}](https://finance.yahoo.com/quote/{sym:})'.format(sym=symbol))
-col2.markdown('[Download image]({})'.format(plot_url))
+    # plot
+    f = plt.figure()
+    f.set_figwidth(10)
+    f.set_figheight(8)
+    plt.xlabel('Date')
+    plt.ylabel('Portfolio Value')
+    stockline, = plt.plot(worthdf['TimeStamp'], worthdf['stock_value'], label='stock', linewidth=0.75)
+    totalline, = plt.plot(worthdf['TimeStamp'], worthdf['value'], label='stock+dividend', linewidth=0.75)
+    xticks, _ = plt.xticks(rotation=90)
+    step = len(xticks) // 10
+    plt.xticks(xticks[::step])
+    plt.legend([stockline, totalline], ['stock', 'stock+dividend'])
+    col1.pyplot(f, height=30)
 
-# Data display
-st.title('Data')
-st.markdown('[Download Excel]({})'.format(spreadsheet_url))
-st.dataframe(worthdf)
+    # inference
+    col2.title('Inference')
+    col2.write('yield = {:.4f} (annually {:.2f}%)'.format(r, convert_expreturn_to_annualreturn(r)*100))
+    col2.write('volatility = {:.4f}'.format(sigma))
+    col2.write('downside risk = {:.4f}'.format(downside_risk))
+    col2.write('upside risk = {:.4f}'.format(upside_risk))
+    if beta is not None:
+        col2.write('beta (w.r.t. {}) = {:.4f}'.format(index, beta))
+    col2.write('Name: {}'.format(allsymbol_info[symbol]['description']))
+    col2.markdown('Symbol: [{sym:}](https://finance.yahoo.com/quote/{sym:})'.format(sym=symbol))
+    col2.markdown('[Download image]({})'.format(plot_url))
+
+    # Data display
+    st.title('Data')
+    st.markdown('[Download Excel]({})'.format(spreadsheet_url))
+    st.dataframe(worthdf)
